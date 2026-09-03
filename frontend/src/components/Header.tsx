a620afaf698a05e9417646dcd331e0dd56a7ad6b
Stefan Andersson <stefan.andersson@sveki.se>
2026-09-04 00:50:57 +0200

    fix: adjust header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,39 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
-import { Link } from 'react-router'
+import { Link, useLocation } from 'react-router'
 import { AppearancePicker } from './AppearancePicker'
 import './Header.css'
 
 export function Header() {
+  const location = useLocation()
+  const isAdminSection = location.pathname.startsWith('/admin')
+
   return (
     <header className="app-header">
-      <Link to="/" className="app-header__brand">
-        Formout
-      </Link>
+      <div className="app-header__start">
+        <Link to="/" className="app-header__brand">
+          Formout
+        </Link>
+        <nav className="app-header__tabs" aria-label="Huvudnavigering">
+          <Link to="/" className="app-header__tab" data-active={!isAdminSection || undefined}>
+            Fyll i
+          </Link>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button type="button" className="app-header__tab">
+                Skapa
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <Link to="/admin" className="app-header__tab" data-active={isAdminSection || undefined}>
+              Skapa
+            </Link>
+          </SignedIn>
+        </nav>
+      </div>
       <div className="app-header__actions">
         <AppearancePicker />
-        <SignedOut>
-          <SignInButton mode="modal">
-            <button type="button" className="app-header__admin-link">
-              Admin
-            </button>
-          </SignInButton>
-        </SignedOut>
         <SignedIn>
-          <Link to="/admin" className="app-header__admin-link">
-            Admin
-          </Link>
           <UserButton />
         </SignedIn>
       </div>
